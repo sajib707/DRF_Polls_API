a09d59a7d1017906c8727838b32fd13ff90d9244
Sajib Mahmud <mdsajibmahmud71@gmail.com>
2023-08-08 11:54:40 +0600

    Added authentication

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -3,7 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Poll, Choice
-from .serializers import PollSerializer, VoteSerializer, ChoiceSerializer
+from .serializers import PollSerializer, VoteSerializer, ChoiceSerializer, UserSerializer
+from django.contrib.auth import authenticate
+
 
 class PollList(APIView):
     def get(self, request):
@@ -38,3 +40,23 @@ class CreateVote(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote, UserCreate, LoginView
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
-
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
